3e22522fe9c940f364639d46ceafba524eeb1c17
Mr.park <20261107@dsm.hs.kr>
2026-07-13 20:01:47 +0900

    flutter commit

--- lib/components/list_tile_component.dart
+++ lib/components/list_tile_component.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/material.dart';
 
 class CustomListTile extends StatelessWidget {
+  final String icontext;
   final String text;
   final String smalltext;
-  const CustomListTile({super.key, required this.text, required this.smalltext});
+
+  const CustomListTile({
+    super.key,
+    required this.text,
+    required this.smalltext,
+    required this.icontext,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: CircleAvatar(
-        radius: 10.0,
-      ),
-      title: Text(
-          text,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
+        backgroundColor: const Color(0xFF42542A),
+        radius: 18,
+        child: Text(
+            icontext,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+          ),
         ),
       ),
-
+      title: Row(
+        children: [
+          Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(width: 8),
+          Text(
+            smalltext,
+            style: TextStyle(
+              color: Colors.grey.withAlpha(128),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
