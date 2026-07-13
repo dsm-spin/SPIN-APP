25a07d3dbe80d4d15de942f0520c88df49a65880
cksckscks1008 <hsc9611@gmail.com>
2026-07-13 19:47:30 +0900

    Fix asset path for logo in splash.dart
    로고 이미지 경로 수정

--- lib/splash/splash.dart
+++ lib/splash/splash.dart
@@ -16,7 +16,7 @@ class Splash extends StatelessWidget {
         children: [
           const SizedBox(height: 320),
           SvgPicture.asset(
-            'lib/assets/images/logo.svg',
+            'assets/images/logo.svg',
             width: 120,
             height: 120,
           ),
