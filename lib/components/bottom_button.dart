9443edaaec557c7970129fa7e7a11f97e2d4f442
cksckscks1008 <hsc9611@gmail.com>
2026-07-13 17:07:15 +0900

    feature: splash페이지 제작
    splash 페이지 제작 및 앱 버튼 컴포넌트 제작

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:spin_app/theme/colors.dart';
+
+class BottomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+
+  const BottomButton({
+    super.key,
+    required this.text,
+    required this.onTap
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 8),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: AppColors.button,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          alignment: Alignment.center,
+          child: TextButton(
+            onPressed: onTap,
+            child: Text(
+              textAlign: TextAlign.center,
+              text,
+              style: TextStyle(
+                color: AppColors.background,
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+
+          ),
+        ),
+      ),
+    );
+  }
+}
